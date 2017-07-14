c72ecce93e8d85616c6ea47a5e09e82a83f64e67
yaoyi11 <767248521@qq.com>
2017-07-14 13:39:01 +0800

    命令行解析

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
 
 
 @app.route('/')
@@ -7,5 +11,10 @@ def index():
     return '<h1>Hello Everyone!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
